fd3732662acd4a218176fb85b35250a7f89f6614
Adrien Maillol <ama@zenexity.com>
2014-03-01 16:47:14 +0100

    test static bodies

--- test/physics/WorldSpec.scala
+++ test/physics/WorldSpec.scala
@@ -37,17 +37,21 @@ class WorldSpec extends Specification {
 
       lst.size === 1
     }
-    "update bodies positions" in running(FakeApplication()) {
+    "update dynamic bodies positions" in running(FakeApplication()) {
       implicit val ec: ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
       val world = Akka.system.actorOf(Props[World])
 
       world ! NewBody(box.copy(velocity = V2(100, 100)))
+      world ! NewBody(box.copy(id="ds", static = true))
       world ! FakeTick(10)
 
       val lst = Await.result((world ? GetBodies).mapTo[Map[String, Body]], Duration.Inf)
 
-      lst.head._2.position.x !== box.position.x
-      lst.head._2.position.y !== box.position.y
+      lst("d1").position.x !== box.position.x
+      lst("d1").position.y !== box.position.y
+
+      lst("ds").position.x mustEqual(box.position.x)
+      lst("ds").position.y mustEqual(box.position.y)
     }
     "fall down because of gravity" in running(FakeApplication()) {
       implicit val ec: ExecutionContext = play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -55,10 +59,14 @@ class WorldSpec extends Specification {
 
       world ! NewBody(box.copy(velocity = V2(0, 0), acceleration= V2(0, 0)))
       world ! NewBody(bigBox.copy(velocity = V2(0, 0), acceleration= V2(0, 0)))
+      world ! NewBody(box.copy(id="ds", static = true))
       world ! FakeTick(10)
       world ! FakeTick(10)
 
       val lst = Await.result((world ? GetBodies).mapTo[Map[String, Body]], Duration.Inf)
+
+      lst("ds").position.x mustEqual(box.position.x)
+      lst("ds").position.y mustEqual(box.position.y)
 
       lst("d1").position.y must beLessThan(box.position.y)
       lst("d2").position.y must beLessThan(bigBox.position.y)
